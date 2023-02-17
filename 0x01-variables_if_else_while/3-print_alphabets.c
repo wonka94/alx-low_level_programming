e0a33fafe7d6532460cdf31d9c12e9a0f37db649
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-02-17 23:25:13 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*print alphabets in both small and capital letters
+*add new line
+*Return: 0.
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	printf("\n");
+
+	return (0);
+}
