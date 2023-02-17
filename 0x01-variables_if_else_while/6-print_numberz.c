dfe8e52955ca18c5d20b02a3ccc2a2e7db10b6da
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-02-17 23:52:37 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*print numbers in base 10
+*Return: 0.
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+
+	putchar("\n");
+
+	return (0);
+}
