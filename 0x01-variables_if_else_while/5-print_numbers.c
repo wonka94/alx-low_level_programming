f36407a61aba05483962eee8c2a90869783f68fb
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-02-17 23:47:51 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*print numbers in base 10
+*Return: 0.
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d ", i);
+	}
+
+	Return (0);
+}
