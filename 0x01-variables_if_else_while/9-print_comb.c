ee5b05d6fdc6c3d3ad7b9e307769d71ae587961c
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-02-18 01:16:55 +0000

    Patience, persistence and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar('0' + i);
 
-		if (i != 8)
+		if (i != 9)
 		{
 			putchar(',');
 			putchar(' ');
