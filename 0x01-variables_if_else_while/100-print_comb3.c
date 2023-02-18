d06462eaa492384fed6c623509405123a236a35d
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-02-18 01:00:02 +0000

    Inventing is a combonation of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int i, j;
 
-	for (int i = 0; i < 8; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (int j = i + 1; j < 9; j++)
+		for (j = i + 1; j < 10; j++)
 		{
 			putchar('0' + i);
 			putchar('0' + j);
