8a19d033261136f9faf2864fe6a5e5d976688eb0
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-02-21 22:55:43 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - check if alphabet is lowercase
+ *
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
