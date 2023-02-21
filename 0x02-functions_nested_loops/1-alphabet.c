328a23df7f53b3576d4815088dc197cf5b94b85e
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-02-21 21:59:44 +0000

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+* Print alphabets in lowercase
+* Return: 0
+*/
+
+void print_alphabet(void) {
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
