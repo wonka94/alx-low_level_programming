d978e541c599e1e7f963899e7bd4f9486d5655e2
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-02-22 02:13:02 +0000

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two numbers and print the result
+ * @a: first value
+ * @b: second value
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
