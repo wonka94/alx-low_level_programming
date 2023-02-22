867483c902ed4fbb5648ab3da2d5752c76defda7
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-02-22 02:49:51 +0000

    The World looks like a multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,50 +8,42 @@
 
 void print_times_table(int n)
 {
-	int a;
-	int b;
-	int c;
+	int i, j, k;
 
-	if (n > 15 || n < 0)
+	if (n >= 0 && n <= 15)
 	{
-		return;
-	}
-	else
-	{
-		for (a = 0; a <= n; a++)
+		for (i = 0; i <= n; i++)
 		{
-			for (b = 0; b <= n; b++)
+			for (j = 0; j <= n; j++)
 			{
-				c = a * b;
-				if ((c / 10) == 0)
+				k = j * i;
+				if (j == 0)
 				{
-					if (b == 0)
-					{
-						_putchar ('0');
-					}
-					if (b != 0)
-					{
-						_putchar (' ');
-						_putchar ((c % 10) + '0');
-					}
-					if (b < 9)
-					{
-						_putchar(',');
-						_putchar (' ');
-					}
-				}
-				else
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
 				{
-					_putchar ((c / 10) + '0');
-					_putchar ((c % 10) + '0');
-					if (b < 9)
-					{
-						_putchar(',');
-						_putchar (' ');
-					}
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
-			_putchar ('\n');
+			_putchar('\n');
 		}
 	}
 }
