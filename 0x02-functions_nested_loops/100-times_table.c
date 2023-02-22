071cd6cdd15c661948e5d2c9a8ec8187de33ecdc
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-02-22 02:42:41 +0000

    The World looks like a multiplication table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * times_table -prints tables
+ * @n: value
+ * Return: 0
+ */
+
+void times_table(int n)
+{
+	int a;
+	int b;
+	int c;
+
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+	else
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+				if ((c / 10) == 0)
+				{
+					if (b == 0)
+					{
+						_putchar ('0');
+					}
+					if (b != 0)
+					{
+						_putchar (' ');
+						_putchar ((c % 10) + '0');
+					}
+					if (b < 9)
+					{
+						_putchar(',');
+						_putchar (' ');
+					}
+				}
+				else
+				{
+					_putchar ((c / 10) + '0');
+					_putchar ((c % 10) + '0');
+					if (b < 9)
+					{
+						_putchar(',');
+						_putchar (' ');
+					}
+				}
+			}
+			_putchar ('\n');
+		}
+	}
+}
