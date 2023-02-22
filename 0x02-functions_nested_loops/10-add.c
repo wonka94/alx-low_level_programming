c870beff914ae40dcf9c6cfbba25d84b0676cae7
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-02-22 02:28:13 +0000

    Battery street

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,6 +4,7 @@
  * add - add two numbers and print the result
  * @a: first value
  * @b: second value
+ * Return: 0
  */
 
 int add(int a, int b)
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print natural numbers up to 98
+ * @n: number to start counting from
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+}
