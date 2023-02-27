08c6ef98803fcec9e904134bbcba00870eaa1596
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-02-27 15:19:41 +0000

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- *_strlen - get length of a string
- *@s: A pointer to string to get length
- *Return: length of the string
+ * _strlen - get length of a string
+ * @s: A pointer to string to get length
+ * Return: length of the string
  */
 
 int _strlen(char *s)
 {
-    return (strlen(*s));
+	int u;
+
+	u = 0;
+	while (s[u] != '\0')
+	{
+		u++;
+	}
+	return (u);
 }
