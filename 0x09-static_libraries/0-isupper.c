eddcb008ffdbd04b01746b900c7c0a8d754f7ac6
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-03-10 21:45:32 +0000

    isupper

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *_strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
+ *
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (0);
+}
