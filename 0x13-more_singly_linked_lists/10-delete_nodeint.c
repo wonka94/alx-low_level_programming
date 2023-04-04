5b3e6d26dcf5ffc1b54856e2790f310918be782f
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-04-04 18:55:01 +0000

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *head_copy = *head;
 	unsigned int i;
 
-	if (*head == NULL)
+	if (head_copy == NULL || head_copy->next || NULL)
 	{
 		return (-1);
 	}
