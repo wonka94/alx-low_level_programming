ae4ef624774a5a2563d1c622460c8e10320c9538
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-04-04 10:44:48 +0000

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,17 +10,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (head == NULL)
+	if (head != NULL)
 	{
-        	return;
+		while (*head)
+		{
+			tmp = (*head)->next;
+			free(*head);
+			*head = tmp;
+		}
 	}
-
-	while (*head)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-	}
-
-	head = NULL;
+	*head = NULL;
 }
