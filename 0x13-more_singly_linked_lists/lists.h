1289499705c146add4000077ad707015a66a1cbf
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-04-04 09:13:39 +0000

    Update header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,19 @@
 
 #include <stdlib.h>
 
+/**
+ * struct listint - singly linked list
+ * @n: integer
+ * @next: next node
+ * Description: singly linked list node structure
+ */
+
+typedef struct listint
+{
+	int n;
+	struct listint *next;
+} listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 
