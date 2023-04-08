1a78f7c84192166d9f2f0a20eae9a8a5bdd1f8a2
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-04-08 23:49:14 +0000

    Thread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the contents
+ * @filename: name of file to be read.
+ * @letters: number of letters to be printed.
+ * Return: content of file else return 0.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	ssize_t next_read, next_write;
+	char *buffer;
+
+	if (!filename)
+	{
+		return (0);
+	}
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+
+	next_read = read(fd, buffer, letters);
+	next_write = write(STDOUT_FILENO, buffer, nrd);
+
+	close(file);
+
+	free(buffer);
+
+	return (next_write);
+}
