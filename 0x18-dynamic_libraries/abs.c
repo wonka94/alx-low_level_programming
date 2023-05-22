53424ca42e74fb1d655faeff5ab50c0ccdca84fb
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-05-22 22:19:55 +0000

    Compute absolute value

--- 0x18-dynamic_libraries/abs.c
+++ 0x18-dynamic_libraries/abs.c
@@ -2,11 +2,11 @@
 
 /**
  * _abs - absolute value of parameter
- * 
- * @n: number to compute absolute value 
- * Return: absolute value of n 
+ *
+ * @n: number to compute absolute value
+ * Return: absolute value of n
  */
 int _abs(int n)
 {
-	return n < 0 ? -n : n;
+	return (n < 0 ? -n : n);
 }
