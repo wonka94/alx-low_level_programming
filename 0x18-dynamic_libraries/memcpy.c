b8a830ffabf3e447a0896fc67d229fcad17677d9
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-05-22 22:45:19 +0000

    Copy memory

---
+++ 0x18-dynamic_libraries/memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: pointer to the destination memory area
+ * @src: pointer to the source memory area
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the destination memory area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *d = dest;
+
+	while (n--)
+		*d++ = *src++;
+
+	return (dest);
+}
