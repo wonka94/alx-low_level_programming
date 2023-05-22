881013c21bebadeef72a0e131e3685a9b2006974
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-05-22 22:14:26 +0000

    Check alphabet

---
+++ 0x18-dynamic_libraries/isalpha.c
@@ -0,0 +1,14 @@
+include "main.h"
+
+/**
+ * _isalpha - check if a character is an alphabet
+ * @c: character to be checked
+ *
+ * Return: 1 if c is a alphabet, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
